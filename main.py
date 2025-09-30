7637a065d1480167f0559af31669054feeb0ae0f
dhruvprajapatitecblic <dhruvprajapati.tecblic@gmail.com>
2025-09-30 12:40:00 +0300

    updating the sript

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import findspark
-findspark.init("C:/spark")
-
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, lit, coalesce
 
@@ -10,30 +7,31 @@ from pyspark.sql.functions import col, lit, coalesce
 spark = SparkSession.builder \
     .appName("Dynamic_ETL") \
     .enableHiveSupport() \
+    .config("spark.jars", "/data/jars/mysql-connector-j-8.0.31.jar") \
     .getOrCreate()
 
 # ==============================
 # STEP 3: Load source CSV data
 # ==============================
-source_csv_path = "actual.csv"
+source_csv_path = "/data/SOURCE_METADATA.csv"
 data_df = spark.read.format("csv") \
     .option("header", "true") \
     .option("inferSchema", "true") \
     .load(source_csv_path)
-hive_table = "ingestion_metadata.metadata"
-data_df.write.mode("append").insertInto(hive_table)
+#hive_table = "ingestion_metadata.metadata"
+#data_df.write.mode("append").insertInto(hive_table)
 
 # ==============================
 # STEP 6: Write to MySQL
 # ==============================
 mysql_url = "jdbc:mysql://10.21.18.51:3306/ingestion_metadata"
 mysql_properties = {
-    "user": "gold_metadata",
+    "user": "bronze_metadata",
     "password": "Tecblic@987",
     "driver": "com.mysql.cj.jdbc.Driver"
 }
 
-mysql_table = "metadata"
+mysql_table = "source_metadata"
 data_df.write.jdbc(url=mysql_url, table=mysql_table, mode="append", properties=mysql_properties)
 
 # ==============================
